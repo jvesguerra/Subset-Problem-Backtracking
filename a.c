3bb69adb1aaa44cb41033e6694e8d73c7652d00b
jvesguerra <jvesguerra@up.edu.ph>
2023-06-02 15:40:16 +0800

    working subset

--- a.c
+++ a.c
@@ -70,7 +70,7 @@ int main(){
 				for(j = candidate_size; j >= 1; j--){
 					nopts[move]++;
 					option[move][nopts[move]] = candidate[j-1];
-					//printf("nopts[move] %i candidate %i\n", nopts[move], candidate[j]);           
+					//printf("nopts[move] %i candidate %i\n", nopts[move], candidate[j-1]);           
 				}
 			}
 			else{
@@ -84,35 +84,57 @@ int main(){
 						//printf("nopts[move] %i candidate %i\n", nopts[move], candidate[j]);
 					}else{
 						int set1_sum = 0;
-						int *arr;
+						int set2_sum = 0;
+						int *arr, *arr2;
 						int counter = 0;
+						int counter2 = 0;
 						arr = (int*)calloc(candidate_size, sizeof(int));
+						arr2 = (int*)calloc(candidate_size, sizeof(int));
 						int checker;
+						int flag;
 
 						for(i=1;i<move;i++){
 							//printf("%d ",option[i][nopts[i]]);
-							//printf("\n");
 							set1_sum += option[i][nopts[i]];
 
 							arr[counter] = option[i][nopts[i]];
 							counter++;
 						}
-						// for(int k=0;k<counter;k++)
-						// 	printf("%2i",arr[k]);
-						printf("\n");						
-						//printf("\n%d\n",set1_sum);
-
-						printf("Remaining numbers: ");
-						for(int y = 0; y < candidate_size; y ++){
-							for(int x =0; x < counter; x++){
-								if(candidate[y] != arr[x]){
-									printf("%d ",candidate[y]);
-								}
-							} 
-						}
+						//printf("\n");						
 
 						
-						// printf("\n");
+						for(int y = 0; y < candidate_size; y ++){
+							flag = 0;
+							for(int x =0; x < counter; x++){
+								if(candidate[y] == arr[x]){
+									flag = 1;
+									break;
+								}
+							} 
+							if (flag == 0) {
+								//printf("%d ",candidate[y]);
+
+								set2_sum += candidate[y];
+
+								arr2[counter2] = candidate[y];
+								counter2++;
+								}
+						}
+						
+						if(set2_sum == set1_sum){
+							// printf("SET1: SUM %d ",set1_sum);
+							// printf("SET2: %d \n",set2_sum);
+							printf("{ ");
+							for(int pt1 = 0; pt1 < counter; pt1++)
+								printf("%2i", arr[pt1]);
+							printf(" }  { ");
+							for(int pt2 = 0; pt2 < counter2; pt2++)
+								printf("%2i", arr2[pt2]);
+							printf(" }");
+
+							printf("\n");
+						}
+						
 						break;
 					}							
 				}
